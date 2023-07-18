615639ef22eee9df9ee2ca9eb97af75d98dc3796
ernesthenry <henry38ernest@gmail.com>
2023-07-18 15:34:52 +0300

    Login API

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -57,3 +57,33 @@ auth_blueprint.add_url_rule(
     view_func=registration_view,
     methods=['POST']
 )
+
+
+
+class LoginAPI(MethodView):
+    """
+    User Login Resource
+    """
+    def post(self):
+        # get the post data
+        post_data = request.get_json()
+        try:
+            # fetch the user data
+            user = User.query.filter_by(
+                email=post_data.get('email')
+              ).first()
+            auth_token = user.encode_auth_token(user.id)
+            if auth_token:
+                responseObject = {
+                    'status': 'success',
+                    'message': 'Successfully logged in.',
+                    'auth_token': auth_token.decode()
+                }
+                return make_response(jsonify(responseObject)), 200
+        except Exception as e:
+            print(e)
+            responseObject = {
+                'status': 'fail',
+                'message': 'Try again'
+            }
+            return make_response(jsonify(responseObject)), 500
