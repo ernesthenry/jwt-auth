9309b85db1e77b153dcae855336e378e4f65aec8
ernesthenry <henry38ernest@gmail.com>
2023-07-18 16:06:23 +0300

    Refactor

--- project/server/models.py
+++ project/server/models.py
@@ -1,5 +1,8 @@
-import datetime
+# project/server/models.py
+
+
 import jwt
+import datetime
 
 from project.server import app, db, bcrypt
 
@@ -22,7 +25,6 @@ class User(db.Model):
         self.registered_on = datetime.datetime.now()
         self.admin = admin
 
-
     def encode_auth_token(self, user_id):
         """
         Generates the Auth Token
@@ -40,25 +42,28 @@ class User(db.Model):
                 algorithm='HS256'
             )
         except Exception as e:
-            return e    
-        
-
+            return e
 
     @staticmethod
     def decode_auth_token(auth_token):
         """
-        Decodes the auth token
+        Validates the auth token
         :param auth_token:
         :return: integer|string
         """
         try:
             payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-            return payload['sub']
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
-        
+
+
 class BlacklistToken(db.Model):
     """
     Token Model for storing JWT tokens
@@ -74,4 +79,13 @@ class BlacklistToken(db.Model):
         self.blacklisted_on = datetime.datetime.now()
 
     def __repr__(self):
-        return '<id: token: {}'.format(self.token)       
+        return '<id: token: {}'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
